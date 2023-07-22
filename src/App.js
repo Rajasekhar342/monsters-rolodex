8180c9d52ee4d2cbf3ceb6f18e32cf806d414f11
Rajasekhar <rajasekharbevara341@gmail.com>
2023-07-22 14:57:25 +0530

    filtering items correctly and storing original data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super()
     this.state = {
       monsters: [],
+      searchField: '',
     }
   }
 
@@ -26,6 +27,9 @@ class App extends Component {
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField)
+    })
     return (
       <div className='App'>
         <input
@@ -33,17 +37,15 @@ class App extends Component {
           type='search'
           placeholder='search monsters'
           onChange={(event) => {
-            const searchString = event.target.value.toLowerCase()
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLowerCase().includes(searchString)
-            })
+            const searchField = event.target.value.toLowerCase()
+
             this.setState(() => {
-              return { monsters: filteredMonsters }
+              return { searchField }
             })
           }}
         />
 
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return <h1 key={monster.name}>{monster.name}</h1>
         })}
       </div>
